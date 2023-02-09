b5fa195960e5a9530f732e88d7094889a93a8896
Bala Eithesh <115264831+balaeithesh@users.noreply.github.com>
2023-02-09 23:46:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 var dinosaur = document.getElementById("dinosaur")
 var cactus = document.getElementById("cactus")
+var button = document.getElementById("button")
 
-function black(){
-    setTimeout(()=>{
-        if(dinosaur.classList != 'jump'){
+function black() {
+    setTimeout(() => {
+        if (dinosaur.classList != 'jump') {
             dinosaur.classList.add('jump');
 
-        setTimeout(() => {
-            dinosaur.classList.remove('jump');
-            },300);
+            setTimeout(() => {
+                dinosaur.classList.remove('jump');
+            }, 300);
         }
     })
 }
 
-document.addEventListener('keydown',()=>{
-    black();
+button.addEventListener('click', () => {
+    cactus.classList.add('run');
+})
+
+document.addEventListener('keydown', (e) => {
+    if (e.code == "Space") {
+        black();
+    }
+
 })
 
 let gameover = setInterval(() => {
-let dinosaurcollide = parseInt(window.getComputedStyle(dinosaur).getPropertyValue('top'));
-let cactuscollide = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
-if(cactuscollide<30 && cactuscollide>0 && dinosaurcollide>=150){
-    alert('Game Over');
-}
+    let dinosaurcollide = parseInt(window.getComputedStyle(dinosaur).getPropertyValue('top'));
+    let cactuscollide = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
+    if (cactuscollide < 50 && cactuscollide > 0 && dinosaurcollide >= 130) {
+        alert('❌GAME OVER❌');
+    }
 }, 10);
